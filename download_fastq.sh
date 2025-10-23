40c15f1a730f8b521c195da9a87d4fa6b2a2586d
Shreya Venkatesh <shve3678@colorado.edu>
2025-10-23 16:13:12 -0600

    reorganized

--- download_fastq.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir /scratch/Users/shve3678/day2-workshop/fastq
-rsync /scratch/Shares/public/sread2025/data_files/day4/fastq/chr21Eric_repA.RNA.end2.fastq /scratch/Users/shve3678/day2-workshop/fastq
--- ena-file-download-selected-files-20250718-1710.sh
+++
@@ -1,1 +0,0 @@
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR127/008/SRR1272928/SRR1272928_1.fastq.gz
